1d0c4bc419dc15220753f0744047662e01e59382
Sarah Moran <s.l.a.moran@gmail.com>
2022-12-08 20:36:37 +0000

    passed tests on up to 3 years for humanCatDogYears - need to work on from age 4 onwards

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,6 +1,23 @@
 const humanCatDogYears = (number) => {
+  if (number === 0) {
+    return [number, number, number];
+}
+
+  if (number === 1) {
+    return [number,number + 14,number + 14]
+}
+
+if (number === 2){
+    return [number,number + 22,number + 22]
+}
+
+if (number ===3) {
+    return [number, number +25, number +26]
+}
+
 
     return [number, 56, 64]
 };
+
 
 module.exports = humanCatDogYears;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,8 +1,24 @@
 const { humanCatDogYears } = require("../src");
 
-  describe("humanCatDogYears", () => {
-    it ("returns an array of three numbers of human age, cat age and dog age when passed human age", () => {
-        expect(humanCatDogYears(10)).toEqual([10,56,64]);
-    });
-   
-    });
+describe("humanCatDogYears", () => {
+  it("returns an array of three numbers of human age, cat age and dog age when passed human age", () => {
+    expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
+    //expect(humanCatDogYears(20)).toEqual([20, , ]);
+  });
+
+  it("returns an array of three numbers of human age, cat age and dog age when passed 0 as human age", () => {
+    expect(humanCatDogYears(0)).toEqual([0, 0, 0]);
+  });
+
+  it("returns an array of three numbers of human age, cat age and dog age when passed 1 as human age", () => {
+    expect(humanCatDogYears(1)).toEqual([1, 15, 15]);
+  });
+
+  it("returns an array of three numbers of human age, cat age and dog age when passed 2 as human age", () => {
+    expect(humanCatDogYears(2)).toEqual([2, 24, 24]);
+  });
+
+  it("returns an array of three numbers of human age, cat age and dog age when passed 3 or over as human age", () => {
+    expect(humanCatDogYears(3)).toEqual([3, 28, 29]);
+  });
+});
