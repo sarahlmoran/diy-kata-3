d01757d05388a97c30250986ed76776e76d5e4eb
Sarah Moran <s.l.a.moran@gmail.com>
2022-12-08 10:17:14 +0000

    FizzBuzz DIY kata completed with walkthrough on track

--- test/kata1.fizzBuzz.test.js
+++ test/kata1.fizzBuzz.test.js
@@ -1,11 +1,23 @@
 const { fizzBuzz } = require("../src");
 
 describe("fizzBuzz", () => {
-  test("returns Fizz when passed a multiple of 3", () => {});
+  it("returns Fizz when passed a multiple of 3", () => {
+    expect(fizzBuzz(3)).toBe("Fizz");
+  })
 
-  test("returns Buzz when passed a multiple of 5", () => {});
 
-  test("returns FizzBuzz when passed a multiple 3 and 5", () => {});
+  it("returns Buzz when passed a multiple of 5", () => {
+    expect(fizzBuzz(5)).toBe("Buzz");
+  });
 
-  test("returns the number when it isn't a multiple of 3 or 5", () => {});
+  it("returns FizzBuzz when passed a multiple 3 and 5", () => {
+    expect(fizzBuzz(15)).toBe("FizzBuzz");
+    expect(fizzBuzz(30)).toBe("FizzBuzz");
+    expect(fizzBuzz(90)).toBe("FizzBuzz");
+  });
+
+  it("returns the number when it isn't a multiple of 3 or 5", () => {
+    expect(fizzBuzz(16)).toBe(16);
+    expect(fizzBuzz(43)).toBe(43);
+  });
 });
