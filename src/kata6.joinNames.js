a5238ae88e4047227cb8a17d28d91996ac370944
Sarah Moran <s.l.a.moran@gmail.com>
2022-12-10 16:27:50 +0000

    completed kata 6 and added comments to the function

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,12 @@
-const joinNames = (namesObj) => {};
+const joinNames = (namesObj) => {
+   return namesObj.map(object => object.name).join(", ").replace(/,(?=[^,]+$)/, " &");
+
+};
 
 module.exports = joinNames;
+
+// first I used .map because I wanted to pull the name for each object in the array
+// then in .map() I went one layer down into the object as the objects are in an array
+// once into the object I wanted to get into the name element so used object.name
+// now the names are in a new array I used .join (", ") to turn to a string with commas
+// then to change the last comma to an ampersand I used a regex and .replace function
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,17 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  it("returns string of names, seperated by commas and an ampersand", () => {
+    expect(
+      joinNames([{ name: "Bart" }, { name: "Lisa" }, { name: "Maggie" }])
+    ).toEqual("Bart, Lisa & Maggie");
+    expect(
+      joinNames([
+        { name: "Bart" },
+        { name: "Lisa" },
+        { name: "Maggie" },
+        { name: "Homer" },
+      ])
+    ).toEqual("Bart, Lisa, Maggie & Homer");
+  });
 });
