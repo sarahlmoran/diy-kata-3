e28b016c4cdaaf924a73bb50304c785ad8641fff
Sarah Moran <s.l.a.moran@gmail.com>
2022-12-10 10:18:31 +0000

    prettified the code on humanCatDogYears js and removed an unneccesary if statement of if the number is equal to 3

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,25 +1,19 @@
 const humanCatDogYears = (number) => {
-  const x = (number - 3)
+  const x = number - 3;
 
   if (number === 0) {
     return [number, number, number];
-}
+  }
 
   if (number === 1) {
-    return [number,number + 14,number + 14]
-}
+    return [number, number + 14, number + 14];
+  }
 
-if (number === 2){
-    return [number,number + 22,number + 22]
-}
+  if (number === 2) {
+    return [number, number + 22, number + 22];
+  }
 
-if (number ===3) {
-    return [number, number +25, number +26]
-}
-
-
-    return [number, 28 +(4*x), 29 +(5*x)]
+  return [number, 28 + 4 * x, 29 + 5 * x];
 };
-
 
 module.exports = humanCatDogYears;
