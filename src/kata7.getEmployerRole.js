e80287c26fe580111f57d0e105e435a52acd2892
Sarah Moran <s.l.a.moran@gmail.com>
2022-12-11 12:39:04 +0000

    completed last kata getEmployerRole

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,8 +1,13 @@
 const getEmployerRole = (employeeName, employees) => {
-    return employees.find(object => object)
-};
+  const employee = employees.find((object) => object.name === employeeName);
 
+  return employee.role;
+};
 module.exports = getEmployerRole;
 
 
 // when you input a name it returns the role
+
+//   return employees.map((object) => object)
+//  .find((object1) => object1.role);
+//  return employees.role;
